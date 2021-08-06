b204d3993d045a11677a79ba7a063f8a92cdc64e
Nick Stockton <nstockton@users.noreply.github.com>
2021-08-06 14:40:55 -0700

    Add a py.typed marker file so Mypy will know we have type hints.

--- setup.py
+++ setup.py
@@ -9,18 +9,24 @@ from __future__ import annotations
 # Built-in Modules:
 import pathlib
 import platform
+import sys
 from typing import List
 
 # Third-party Modules:
 from setuptools import setup  # type: ignore[import]
 
 
+if sys.version_info < (3, 7):
+	raise NotImplementedError("Only Python 3.7+ is supported.")
+
+
 NAME: str = "Speechlight"
 DESCRIPTION: str = (
 	"A lightweight Python library providing a common interface to multiple TTS and screen reader APIs."
 )
+KEYWORDS: str = "mud telnet protocol"
 AUTHOR: str = "Nick Stockton"
-VERSION: str = "1.3"
+VERSION: str = "1.4"
 URL: str = "https://github.com/nstockton/speechlight"
 # The directory containing this file
 HERE: pathlib.Path = pathlib.Path(__file__).parent
@@ -35,6 +41,7 @@ if platform.system() == "Windows":
 setup(
 	python_requires=">=3.7",
 	name=NAME,
+	keywords=KEYWORDS,
 	author=AUTHOR,
 	version=VERSION,
 	description=DESCRIPTION,
@@ -43,7 +50,7 @@ setup(
 	url=URL,
 	package_dir={"speechlight": "speechlight"},
 	packages=["speechlight"],
-	package_data={"speechlight": ["speech_libs/*"]},
+	package_data={"speechlight": ["py.typed", "speech_libs/*"]},
 	zip_safe=False,
 	setup_requires=REQUIREMENTS,
 	install_requires=REQUIREMENTS,
@@ -52,8 +59,12 @@ setup(
 	platforms="Posix; MacOS X; Windows",
 	classifiers=[
 		"License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+		"Programming Language :: Python :: 3 :: Only",
 		"Programming Language :: Python :: 3",
 		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python",
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
 		"Operating System :: OS Independent",
